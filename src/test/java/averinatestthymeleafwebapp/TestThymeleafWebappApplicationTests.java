f9716b21e02606d926bebe13daa1bfc52388c08f
Averina Ola <-->
2023-09-16 18:10:31 +0500

    --

---
+++ src/test/java/averinatestthymeleafwebapp/TestThymeleafWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package averinatestthymeleafwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestThymeleafWebappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
